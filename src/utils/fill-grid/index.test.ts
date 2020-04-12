83ddff5b0121b4b4bfd7073ae976d9367aaafbbf
honningsvag <xpmueller@gmail.com>
2020-04-12 00:55:25 -0400

    test and fix bug

---
+++ src/utils/fill-grid/index.test.ts
@@ -0,0 +1,42 @@
+import { GRID } from "typings";
+import fillGrid from "./index";
+
+describe("fillGrid", () => {
+  it("fills an empty", () => {
+    const grid: GRID = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    fillGrid(grid);
+    for (let row in grid) {
+      for (let col in grid) expect(grid[row][col]).toBeGreaterThanOrEqual(1);
+      for (let col in grid) expect(grid[row][col]).toBeLessThanOrEqual(9);
+    }
+  });
+
+  it("fills a valid and partially filled grid", () => {
+    const grid: GRID = [
+      [1, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    ];
+    fillGrid(grid);
+    for (let row in grid) {
+      for (let col in grid) expect(grid[row][col]).toBeGreaterThanOrEqual(1);
+      for (let col in grid) expect(grid[row][col]).toBeLessThanOrEqual(9);
+    }
+  });
+});
