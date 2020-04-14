be6b552705e785fffaca2445f68abf5cd3342224
honningsvag <xpmueller@gmail.com>
2020-04-14 16:57:04 -0400

    initial redux work

---
+++ src/utils/create-grid/index.ts
@@ -0,0 +1,23 @@
+import { GRID } from "typings";
+import { fillGrid } from "utils";
+
+/**
+ * Returns a valid 9x9 sudoku grid
+ */
+let createFullGrid = (): GRID => {
+  const grid: GRID = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  fillGrid(grid);
+  return grid;
+};
+
+export default createFullGrid;
