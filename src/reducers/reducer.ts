413d8ad5779f17ad085de031b40e622287b0e20e
honningsvag <xpmueller@gmail.com>
2020-04-15 23:00:00 -0400

    actions set up redux

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,9 +1,18 @@
 import { AnyAction } from "redux";
+import { IReducer } from "./interfaces";
+import { createFullGrid } from "utils";
+import * as types from "./types";
 
 const initialState = {};
 
-const reducer = (state = initialState, action: AnyAction) => {
+const reducer = (state = initialState, action: AnyAction): IReducer => {
   switch (action.type) {
+    case types.CREATE_GRID:
+      return {
+        ...state,
+        grid: createFullGrid(),
+      };
+
     default:
       return state;
   }
