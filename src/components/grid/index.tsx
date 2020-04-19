1ce93228b3e2a55e3a3b3296008c7c93bb732ea1
honningsvag <xpmueller@gmail.com>
2020-04-18 22:14:10 -0400

    keybindings done

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -5,20 +5,36 @@ import useMousetrap from "react-hook-mousetrap";
 
 import Block from "./block";
 import { Container, Row } from "./styles";
-import { createGrid, IReducer, selectBlock } from "reducers";
-import { INDEX, BLOCK_COORDS } from "typings";
+import { createGrid, IReducer, selectBlock, fillBlock } from "reducers";
+import { INDEX, BLOCK_COORDS, NUMBERS, N } from "typings";
 
 interface IState {
   selectedBlock?: BLOCK_COORDS;
+  selectedValue: N;
 }
 
 const Grid: FC = () => {
-  const state = useSelector<IReducer, IState>(({ selectedBlock }) => ({
-    selectedBlock,
-  }));
+  const state = useSelector<IReducer, IState>(
+    ({ selectedBlock, workingGrid }) => ({
+      selectedBlock,
+      selectedValue:
+        workingGrid && selectedBlock
+          ? workingGrid[selectedBlock[0]][selectedBlock[1]]
+          : 0,
+    })
+  );
 
   const dispatch = useDispatch<Dispatch<AnyAction>>();
   const create = useCallback(() => dispatch(createGrid()), [dispatch]);
+
+  const fill = useCallback(
+    (n: NUMBERS) => {
+      if (state.selectedBlock && state.selectedValue === 0) {
+        dispatch(fillBlock(n, state.selectedBlock));
+      }
+    },
+    [dispatch, state.selectedBlock, state.selectedValue]
+  );
 
   useEffect(() => {
     create();
@@ -64,6 +80,16 @@ const Grid: FC = () => {
       );
     }
   };
+
+  useMousetrap("1", () => fill(1));
+  useMousetrap("2", () => fill(2));
+  useMousetrap("3", () => fill(3));
+  useMousetrap("4", () => fill(4));
+  useMousetrap("5", () => fill(5));
+  useMousetrap("6", () => fill(6));
+  useMousetrap("7", () => fill(7));
+  useMousetrap("8", () => fill(8));
+  useMousetrap("9", () => fill(9));
 
   useMousetrap("down", moveDown);
   useMousetrap("up", moveUp);
